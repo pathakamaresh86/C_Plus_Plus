b96f08a1b767af018886a849132f66b7d413d0d1
Jeetendra Bhattad <bhattad.jeetendra@gmail.com>
2018-08-20 13:23:17 +0530

    Program : Bits Change Detection, updated.

--- C++11/bits_change_detection.cc
+++ C++11/bits_change_detection.cc
@@ -5,15 +5,13 @@ using namespace std;
 int solution(vector<int> &segments)
 {
     int count = -1;
-    int prev = 0;
-    int next = 1;
     if(segments.size() == 0 || segments.size() == 1)
         count = 0;
     else if(segments.size() > 1)
     {
         count = 0;
-        int prev_segment = segments[prev];
-        int next_segment = segments[next];
+        int prev_segment = segments[0];
+        int next_segment;
         int lane_exists = segments[0];
         for(int i = 1; i < segments.size() ; i+=1)
         {
@@ -46,12 +44,12 @@ int solution(vector<int> &segments)
 
 int main()
 {
-    vector<int> segments = {1,10,1,15}; //expected ans 2 : 0001,1010,0001,1111
+    //vector<int> segments = {1,10,1,15}; //expected ans 2 : 0001,1010,0001,1111
     //vector<int> segments = {1,1,1,1}; //expected ans 0 : 0001, 0001, 0001, 0001
     //vector<int> segments = {1,16,4,8}; // expected ans -1 : 0001, 10000,0100, 1000
     //vector<int> segments = {1,2,4,8}; // expected ans 3 : 0001, 0010, 0100, 1000
     //vector<int> segments = {1,3,5,7}; // expected ans 0 : 0001, 0011, 0101, 0111
-    //vector<int> segments = {7,11,10,4};// expected ans 1 : 0111, 1011, 1010,0100
+    vector<int> segments = {7,11,10,4};// expected ans 1 : 0111, 1011, 1010,0100
 
     int laneChangeCount = solution(segments);
 
